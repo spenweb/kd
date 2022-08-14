ca6b61a01fa2d61ff7b81fd9dd2a327143aa4611
Emi Brown <emi@spencerwebservices.com>
2022-08-14 15:21:30 -0700

    Added interactive CLI prompts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 //!  # kd - Korean Drama
 //!
 //! kd helps easily document Korean Dramas making watching Korean Dramas more fun!
-use std::{env, process};
+use std::process;
 
 use clap::{Parser, Subcommand};
+use inquire::{Confirm, CustomType, CustomUserError, Text};
 use kd::{actor::Actor, character::Character, korean::utils, show::Show};
-use num_format::{Buffer, Locale, ToFormattedString, WriteFormatted};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -41,12 +41,12 @@ enum AddCommands {
     /// Add show
     Show {
         /// Name of show
-        #[clap(short, long)]
-        name: String,
+        #[clap(short, long, required(false))]
+        name: Option<String>,
 
         /// Release year of show
-        #[clap(short, long)]
-        release_year: i16,
+        #[clap(short, long, required(false))]
+        release_year: Option<i16>,
     },
 
     /// Add actor
@@ -120,8 +120,7 @@ fn main() {
         }
         Some(Commands::Add { command }) => match command {
             Some(AddCommands::Show { name, release_year }) => {
-                let show = Show::new(name, release_year);
-                println!("Added new show: {show}");
+                add_show_controller(name, release_year)
             }
             Some(AddCommands::Actor { name, birth_year }) => {
                 let actor = Actor::new(name, birth_year);
@@ -135,4 +134,50 @@ fn main() {
         },
         None => {}
     }
+}
+
+fn add_show_controller(name: Option<String>, release_year: Option<i16>) {
+    let validated_name: String;
+    let validated_release_year: i16;
+    if let Some(name) = name {
+        validated_name = name;
+    } else {
+        validated_name = Text::new("Show's title:")
+            .with_suggester(&show_suggestor)
+            .prompt()
+            .unwrap();
+    }
+    if let Some(release_year) = release_year {
+        validated_release_year = release_year;
+    } else {
+        validated_release_year = CustomType::new("Show's release year:")
+            .with_error_message("Please enter a valid year")
+            .prompt()
+            .unwrap();
+    }
+    let show = Show::new(validated_name, validated_release_year);
+    if let Ok(true) = Confirm::new(format!("Does this info look correct: {show}").as_str())
+        .with_default(true)
+        .with_help_message("Will save if correct")
+        .prompt()
+    {
+        println!("Added new show: {show}");
+    }
+}
+
+fn show_suggestor(input: &str) -> Result<Vec<String>, CustomUserError> {
+    let input = input.to_lowercase();
+    let shows = vec![
+        "Our Blues",
+        "It's Okay Not To Be Okay",
+        "Crash Landing On You",
+        "Extraordinary Attorney Woo Young Woo",
+    ];
+
+    Ok(shows
+        .iter()
+        .filter(|p| p.to_lowercase().contains(&input))
+        .take(5)
+        .map(|p| String::from(*p))
+        .collect())
 }
